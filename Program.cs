863b451d38802c6920e034d567dc951553183756
Shashank <shashankpateriya4@gmail.com>
2021-07-26 18:09:28 +0530

    [shashank] Add: Frequency of words

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day15_HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table Program");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "to");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            Console.WriteLine("Frequency of to  : " + hash.GetFrequencyOfWords("to"));
+            Console.WriteLine("Frequency of be  : " + hash.GetFrequencyOfWords("be"));
+            Console.WriteLine("Frequency of or  : " + hash.GetFrequencyOfWords("or"));
+            Console.WriteLine("Frequency of not : " + hash.GetFrequencyOfWords("not"));
+        }
+    }
+}
