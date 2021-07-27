038aa1a9364a047585d44b2ea2d5be2ab8663841
Shashank <shashankpateriya4@gmail.com>
2021-07-27 22:14:37 +0530

    [shashank] Add: Deleting words from sentence

--- Frequency.cs
+++ Frequency.cs
@@ -20,5 +20,6 @@ namespace Day15_HashTables
 
             }
         }
+
     }
 }
--- MyMapNode.cs
+++ MyMapNode.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Day15_HashTables
 {
-    class MyMapNode<K, V>
+    public class MyMapNode<K, V>
     {
         public readonly int size;
         private readonly LinkedList<KeyValue<K, V>>[] items;
@@ -14,14 +14,14 @@ namespace Day15_HashTables
             this.size = size;
             this.items = new LinkedList<KeyValue<K, V>>[size];
         }
-
-
+        // Method to get position of the key in the array using GetHashCode() method
         public int GetArrayPosition(K key)
         {
             int position = key.GetHashCode() % size;
             return Math.Abs(position);
         }
 
+        // Method to get value based on its key
         public V Get(K key)
         {
             int position = GetArrayPosition(key);
@@ -36,6 +36,9 @@ namespace Day15_HashTables
             return default(V);
         }
 
+
+        // Method to add KeyValue pair to the linked list of type KeyValue<K, V>
+
         public void Add(K key, V value)
         {
             int position = GetArrayPosition(key);
@@ -43,6 +46,8 @@ namespace Day15_HashTables
             KeyValue<K, V> item = new KeyValue<K, V>() { key = key, value = value };
             linkedList.AddLast(item);
         }
+
+        // Method to get linked list based on its position
 
         public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
         {
@@ -54,6 +59,30 @@ namespace Day15_HashTables
             }
             return linkedList;
         }
+
+
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+                if (itemFound)
+                {
+                    Console.WriteLine("Item Removed " + foundItem.key);
+                    linkedList.Remove(foundItem);
+                    break;
+                }
+            }
+        }
+
 
         public struct KeyValue<Ke, Va>
         {
