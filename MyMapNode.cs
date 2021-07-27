8dea74389c7abb484013a1fabd5d774699dcdd84
Shashank <shashankpateriya4@gmail.com>
2021-07-27 22:10:25 +0530

    [shashank] Add: Frequency of words in sentence

--- MyMapNode.cs
+++ MyMapNode.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Day15_HashTable
+namespace Day15_HashTables
 {
     class MyMapNode<K, V>
     {
-        private readonly int _size;
+        public readonly int size;
         private readonly LinkedList<KeyValue<K, V>>[] items;
 
         public MyMapNode(int size)
         {
-            this._size = size;
+            this.size = size;
             this.items = new LinkedList<KeyValue<K, V>>[size];
         }
 
         public int GetArrayPosition(K key)
         {
-            int position = key.GetHashCode() % _size;
+            int position = key.GetHashCode() % size;
             return Math.Abs(position);
         }
 
@@ -52,26 +52,6 @@ namespace Day15_HashTable
                 items[position] = linkedList;
             }
             return linkedList;
-        }
-
-        public int GetFrequencyOfWords(V value)
-        {
-            int count = 0;
-            if (items == null)
-            {
-                Console.WriteLine("Hash Table is Empty!");
-                return 0;
-            }
-            for (int i = 0; i < items.Length; i++)
-            {
-                LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(i);
-                foreach (KeyValue<K, V> item in linkedList)
-                {
-                    if (item.value.Equals(value))
-                        count++;
-                }
-            }
-            return count;
         }
 
         public struct KeyValue<Ke, Va>
